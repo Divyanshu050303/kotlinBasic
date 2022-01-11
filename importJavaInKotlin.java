430a812df1e3d46329938b90b60013140752fca8
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-01-12 00:22:38 +0530

    importJavaInKotlin

---
+++ importJavaInKotlin.java
@@ -0,0 +1,12 @@
+public class importJavaInKotlin {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
