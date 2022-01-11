2d364ee1539ded158655d53bcc623089c2982755
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-01-12 00:23:18 +0530

    nullHandling

---
+++ nullHandling.kt
@@ -0,0 +1,6 @@
+fun main(args:Array<String>){
+    //with the help the ? we handle the null value in kotlin or we can use
+    // so many function with the null value like
+    var name:String? =null
+    println("The name is: $name")
+}
