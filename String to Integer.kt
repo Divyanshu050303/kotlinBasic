d5439f6c347738e188648d583ec4232051b634a7
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-01-13 00:07:19 +0530

    String to Integer

---
+++ String to Integer.kt
@@ -0,0 +1,6 @@
+fun main(args:Array<String>){
+    var str:String="5" // Initialize  the number in the string format
+    var num:Int=str.toInt() // we convert the string in to integer with help
+    // ot toInt()Method
+    print(num)
+}
